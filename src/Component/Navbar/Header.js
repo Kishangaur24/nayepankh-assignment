d2e58242eb7139b2d06b109aa97a7c7a3d369973
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-10-27 11:21:11 +0530

    navigation update

--- src/Component/Navbar/Header.js
+++ src/Component/Navbar/Header.js
@@ -3,7 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import img from "../FrontPage/logo.jpg"
+import { useState } from 'react';
+import { animateScroll as scroll ,Link as ScrollLink } from 'react-scroll';
 function Header() {
+  const[click,setClick]=useState(false)
+  function handleClick(){
+    setClick(!click)
+    scroll.scrollToTop()
+  }
   return (
     <Navbar bg="dark" expand="lg" variant="dark" style={{ position: 'sticky', top: '0' }} >
       <Container>
@@ -13,7 +20,9 @@ function Header() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
           <Nav.Link as={NavLink} className='text-white ' to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink}  className='text-white' to="/AboutUs">About us</Nav.Link>
+          <ScrollLink to="/aboutUs" smooth={true} duration={500}>
+          <Nav.Link as={NavLink}  className='text-white' to="/aboutUs"  onClick={handleClick}>About us</Nav.Link>
+          </ScrollLink>
           <Nav.Link as={NavLink} className='text-white' to="/certificate">Our Certificate</Nav.Link>
           <Nav.Link as={NavLink} className='text-white' to="/certificate">News-Paper Recognition</Nav.Link>
           <Nav.Link as={NavLink} className='text-white' to="/certificate">Donate</Nav.Link>
