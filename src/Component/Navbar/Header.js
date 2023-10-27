99690dddc4b1237f82ec25a9c1845f400f80ca94
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2023-10-27 09:56:34 +0530

    logo update

--- src/Component/Navbar/Header.js
+++ src/Component/Navbar/Header.js
@@ -2,16 +2,17 @@ import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import img from "../FrontPage/logo.jpg"
 function Header() {
   return (
     <Navbar bg="dark" expand="lg" variant="dark" >
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <img src={img} height="150px"/>
+        {/* <Navbar.Brand as={NavLink} to="/"><img src='../FrontPage/logo.jpg'/></Navbar.Brand> */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-          <Nav.Link as={NavLink} className='text-white ' to="/Home">Home</Nav.Link>
+          <Nav.Link as={NavLink} className='text-white ' to="/">Home</Nav.Link>
           <Nav.Link as={NavLink}  className='text-white' to="/certificate">About us</Nav.Link>
           <Nav.Link as={NavLink} className='text-white' to="/certificate">Our Certificate</Nav.Link>
           <Nav.Link as={NavLink} className='text-white' to="/certificate">News-Paper Recognition</Nav.Link>
